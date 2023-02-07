1dc5b45da104c7ade6c0c68714586547e3019210
keymar0725 <takahashikeisuke2525@gmail.com>
2023-02-07 14:49:51 +0900

    modified plot every minutes

--- main.py
+++ main.py
@@ -21,18 +21,20 @@ plt_pitch = int(sys.argv[3])    # plot pitch
 if plt_pitch==3600:
     plt_name = "1h"
 elif plt_pitch==1800:
-    plt_name = "30m"
-elif plt_pitch == 1:
-    plt_name = "1m"
+    plt_name = "30min"
+elif plt_pitch==60:
+    plt_name = "1min"
+elif plt_pitch==1:
+    plt_name = "1sec"
 
-ch_inner = int(sys.argv[4])     # 庫内 channel
+ch_env = int(sys.argv[4])     # enviroment channel
 
 
 # Debug
-print("Input file: {0} ({1})".format(os.path.basename(import_file), type(import_file)))
-print("Channel number: {0} ({1})".format(ch_num, type(ch_num)))
+print("Input File: {0} ({1})".format(os.path.basename(import_file), type(import_file)))
+print("Channel Number: {0} ({1})".format(ch_num, type(ch_num)))
 print("Plot Picth: {0} ({1})".format(plt_pitch, type(plt_pitch)))
-print("Inner channel: {0} ({1})".format(ch_inner, type(ch_inner)))
+print("Inner Channel: {0} ({1})".format(ch_env, type(ch_env)))
 out_name = os.path.basename(import_file).rstrip('.CSV')
 print("Output Name: {0} ({1})".format(out_name, type(out_name)))
 
@@ -49,7 +51,7 @@ df = pd.read_csv(import_file, names = col_name,
 # filter DF
 plt_num = int(df.loc[df[1] == 'データ数'].iloc[0,1])
 header_num = int(df.iloc[0,1]) - 7
-_df = df[header_num:]
+_df = df[header_num:]                                  # DF update
 
 if ch_num >= 4:
     _df = _df.drop([1,2], axis=1)
@@ -61,7 +63,7 @@ else:
     _df = _df.dropna(how='any')
     _df = _df[1:-1]
 
-_df = _df[1:].astype(float)
+_df = _df[1:].astype(float)                            # DF update
 
 
 # list plot points
@@ -79,19 +81,31 @@ for i in range(ch_num):
 for ch, j in zip(channels, range(ch_num)):
     for i in range(0, plt_num, plt_pitch):
         a = float(_df.iloc[i,j])
-        if plt_pitch==3600 and j!=ch_inner-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
+        if plt_pitch==3600 and j!=ch_env-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
             print("ch{0} : {1} h".format(j+1, len(ch)))
-        if plt_pitch==1800 and j!=ch_inner-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
+        if plt_pitch==1800 and j!=ch_env-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
             print("ch{0} : {1:.1f} h".format(j+1, len(ch)/2))
+        if plt_pitch==60 and j!=ch_env-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
+            print("ch{0} : {1} min".format(j+1, len(ch)))
+        if plt_pitch==1 and j!=ch_env-1 and a>0 and  len(ch)!=0 and ch[-1]<=0:
+            print("ch{0} : {1} sec".format(j+1, len(ch)))
         ch.append(a)
 
 # Plot init
 fig = plt.figure(figsize=(9, 7), dpi=300)
 ax = fig.add_subplot(111)
-ax.set_title('Defrost Test: {}'.format(out_name), fontsize=18)
-ax.set_xlabel('Time [h]', fontsize=16)
+ax.set_title('Unfreeze Test: {}'.format(out_name), fontsize=18)
 ax.set_ylabel('Temparature [℃]', fontsize=16)
 ax.axhline(y=0, color='red', lw=1, ls='--')
+
+if plt_pitch==3600:
+    ax.set_xlabel('Time [h]', fontsize=16)
+elif plt_pitch==1800:
+    ax.set_xlabel('Time [0.5h]', fontsize=16)
+elif plt_pitch==60:
+    ax.set_xlabel('Time [min]', fontsize=16)
+elif plt_pitch==1:
+    ax.set_xlabel('Time [sec]', fontsize=16)
 
 # Plot
 for ch, names in zip(channels, ch_names):
